7ecb79704ec033a9628d53398a4583181aecc188
ssrs <947164239@qq.com>
2020-11-01 17:55:06 +0800

    七牛云存储插件提交

---
+++ plugin-filestorage/plugin-qiniuyun/src/main/java/com/ssrs/qiniuyun/QiniuyunPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.qiniuyun;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class QiniuyunPlugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
